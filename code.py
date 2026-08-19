2ede152eefbd02e63400a89bf83db0f5eb4e2c97
rafaelramos740 <kingrafae32@gmail.com>
2026-08-19 20:17:10 +0800

    CODE CHALLENGE
    CODE  CHALLENGE

---
+++ code.py
@@ -0,0 +1,8 @@
+star = "\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+star1 = "\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t*"
+star2 = "\t\t\t\t\t\t\n\t\t\t\t*\t\t\t\t\t\t\t\tking rafael ramos\t\t\t\t\t\t\t\t\t*"
+star3 = "\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t*"
+star4 = "\t\t\t\t\n\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+
+
+print(star, star1, star2, star3, star4)
