943171eb1b446edbe82acde8028492a3a205ddf5
rafaelramos740 <kingrafae32@gmail.com>
2026-08-21 20:07:22 +0800

    Delete code.py

--- code.py
+++
@@ -1,8 +0,0 @@
-star = "\t\t\t\t\t\t\t\t\t\t\t\t\t*"
-star1 = "\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t*"
-star2 = "\t\t\t\t\t\t\n\t\t\t\t*\t\t\t\t\t\t\t\tking rafael ramos\t\t\t\t\t\t\t\t\t*"
-star3 = "\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t*"
-star4 = "\t\t\t\t\n\t\t\t\t\t\t\t\t\t\t\t\t\t*"
-
-
-print(star, star1, star2, star3, star4)
